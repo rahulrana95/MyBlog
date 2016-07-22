d07ef256b026ac22257fca3d9870420878a87022
Rahul Rana <3rahul4@gmail.com>
2016-07-22 12:06:13 +0530

    2day 1 change

--- blogApp/settings.py
+++ blogApp/settings.py
@@ -59,8 +59,12 @@ WSGI_APPLICATION = 'blogApp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'blog',
+        'USER': 'rahulrana95',
+        'PASSWORD': '9068390682',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
